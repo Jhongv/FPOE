519d41c91d32a3e2be96ab8526227cc61f2038ff
VictorUnivalle <tabares.victor@correounivalle.edu.co>
2024-04-17 10:41:28 +0200

    cambios hechos de la api

--- unidad 1/Clase6Victor/front/Datos.py
+++ unidad 1/Clase6Victor/front/Datos.py
@@ -10,6 +10,7 @@ def validarNombre(valor):
     if not resultadoN:
         return False
     return True
+
 def eventoVnombre(event):
     global nombre
     if validarNombre(nombre):
@@ -24,6 +25,7 @@ def validarApellido(valorApellido):
     if not resultadoN:
         return False
     return True
+
 def eventoVApellido(event):
     global apellido
     if validarApellido(apellido):
@@ -33,67 +35,71 @@ def eventoVApellido(event):
     labelErrorApellido.config(text=textoVapellido)
 
 def validarEstatura(valor):
-    patronA = re.compile("^\d*$")
-    resultadoN = patronA.match(valor.get()) is not None
+    patronA = re.compile(r"^\d{1,2}(\.\d{0,2})?$")
+    resultadoN = patronA.match(valor) is not None
     if not resultadoN:
         return False
     return True
 
 def eventoVEstatura(event):
     global estatura
-    valor_estatura = txtAtr3.get() 
-    if validarEstatura(valor_estatura):
+    valor_estatura = estatura.get()
+    if not valor_estatura.replace('.', ''):
+        textoVEstatura = "Por favor, ingrese solo números y un punto para los decimales"
+    elif validarEstatura(valor_estatura):
         textoVEstatura=""
     else:
-        textoVEstatura="Estatura no cumple con lo establesido"
+        textoVEstatura="Estatura no cumple con lo establecido"
     lblErrorEstatura.config(text=textoVEstatura)
 
 def validarPeso(valor):
-    patronA = re.compile("^\d*$")
-    resultadoN = patronA.match(valor.get()) is not None
+    patronA = re.compile(r"^\d{1,2}(\.\d{0,2})?$")
+    resultadoN = patronA.match(valor) is not None
     if not resultadoN:
         return False
     return True
 
 def eventoVPeso(event):
     global peso
-    valorPeso = txtAtr4.get() 
-    if validarPeso(valorPeso):
+    valorPeso = peso.get() 
+    if not valorPeso.replace('.', ''):
+        textoVpeso = "Por favor, ingrese solo números y un punto para los decimales"
+    elif validarPeso(valorPeso):
         textoVpeso=""
     else:
-        textoVpeso="Estatura no cumple con lo establesido"
+        textoVpeso="Peso no cumple con lo establecido"
     lblErrorPeso.config(text=textoVpeso)
 
-
 def validarInformacion():
-    nombreV = re.match("^[A-Za-zñÑ ]*$", nombre.get())
-    apellidoV = re.match("^[A-Za-zñÑ ]*$", apellido.get())
-    estaturaV = re.match("^\d*$", estatura.get())  # Corregir esta línea
-    pesoV = re.match("^\d*$", peso.get())
+    nombreV = re.match(r"^[A-Za-zñÑ ]*$", nombre.get())
+    apellidoV = re.match(r"^[A-Za-zñÑ ]*$", apellido.get())
+    estaturaV = re.match(r"^\d{1,2}(\.\d{0,2})?$", estatura.get()).group()
+    pesoV = re.match(r"^\d{1,2}(\.\d{0,2})?$", peso.get()).group()
+
 
     if nombreV and apellidoV and estaturaV and pesoV:
-        messagebox.showinfo("Informacion", "Se guardo correctamente")
-
+        
         data = {
-            "nombre": nombreV.get(),
-            "apellido": apellidoV.get(),
-            "estatura": estaturaV.get(),
-            "peso": pesoV.get()
+            "nombre": nombre.get(),
+            "apellido": apellido.get(),
+            "estatura": estatura.get(),
+            "peso": peso.get()
         }
-        respuesta = requests.post("http://localhost:8000/v1/persona", data)
+        respuesta = requests.post("http://127.0.0.1:8000/v1/persona", data)
         print(respuesta.status_code)
         print(respuesta.content)
+        
+        messagebox.showinfo("Informacion", "Se guardo correctamente")
     else:
-        messagebox.showerror("Informacion", "No se pudo guardar debe confirmar si está correcta")
-
+        messagebox.showerror("Informacion", "No se pudo guardar, confirme si está correcto")
 
 root=Tk()
 root.title("Clase persona")
 root.resizable(0,0)
-nombre=StringVar(root) #Indispenzable para la funcion de validar el nombre sin esto no funciona/Se llama en las linas 12 y 13
+nombre=StringVar(root)
 apellido=StringVar(root)
-estatura=DoubleVar(root)
-peso=DoubleVar(root)
+estatura=StringVar(root)
+peso=StringVar(root)
 
 marcotitulo=LabelFrame(root)
 marcotitulo.grid(row=0, column=1, padx=5, pady=5)
@@ -121,13 +127,12 @@ txtAtr2.grid(row=0, column=1, padx=10, pady=10)
 labelErrorApellido=Label(marcoAtr2, text="", fg="red")
 labelErrorApellido.grid(row=1, column=1)
 
-
 marcoAtr3=LabelFrame(root)
 marcoAtr3.grid(row=1, column=2, padx=5, pady=5)
 
 lblAtr3=Label(marcoAtr3, text="Ingrese el 3er atributo de Cl/Persona(Estatura(Cm)/float)*:")
 lblAtr3.grid(row=0,column=0)
-txtAtr3=Entry(marcoAtr3)
+txtAtr3=Entry(marcoAtr3, textvariable=estatura)
 txtAtr3.grid(row=0, column=1, padx=10, pady=10)
 lblErrorEstatura=Label(marcoAtr3, text="", fg="red")
 lblErrorEstatura.grid(row=1, column=1)
@@ -137,11 +142,11 @@ marcoAtr4.grid(row=2,column=2, padx=5, pady=5)
 
 lblAtr4=Label(marcoAtr4, text="Ingrese el 4to atributo de Cl/Persona(Peso(Kg)/int)*:")
 lblAtr4.grid(row=0,column=0)
-txtAtr4=Entry(marcoAtr4)
+txtAtr4=Entry(marcoAtr4, textvariable=peso)
 txtAtr4.grid(row=0, column=1, padx=10, pady=10)
 lblErrorPeso=Label(marcoAtr4, text="", fg="red")
 lblErrorPeso.grid(row=1, column=1)
-btnGuardar=Button(root, text="Guardar", padx=10, pady=10, command=validarInformacion())
+btnGuardar=Button(root, text="Guardar", padx=10, pady=10)
 btnGuardar.grid(row=3, column=1, padx=10, pady=10)
 
 
